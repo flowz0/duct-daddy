daf33994d967299f2041c66d75f64fc7ef3d00d8
flowz0 <billylflowers@gmail.com>
2025-05-22 18:13:19 -0700

    Project setup

---
+++ app/(pages)/blog/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+  return (
+    <article>
+      <h1>Blog Title</h1>
+    </article>
+  );
+}
---
+++ app/(pages)/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <article>
+      <h1>Blog Posts</h1>
+    </article>
+  );
+}
