4524ef854fd1b8657e6f93689aeced0d599e93af
flowz0 <billylflowers@gmail.com>
2025-06-06 19:59:38 -0700

    separate see the duct daddy and difference

--- app/components/pages/landing/BeforeAfterPics.tsx
+++ app/components/pages/landing/BeforeAfterPics.tsx
@@ -13,9 +13,9 @@ export default function BeforeAfterPics() {
   return (
     <section className="py-32 px-6 max-w-7xl mx-auto ">
       <h2 className="text-3xl font-semibold text-center sm:text-4xl">
-        See the
+        See the Duct Daddy
         <br />
-        Duct Daddy <span className="text-[#0080DB] font-bold">Difference</span>
+        <span className="text-[#0080DB] font-bold">Difference</span>
       </h2>
       <p className="max-w-xs mx-auto text-center mt-4">
         Real photos from homes in Oak Grove, Blue Springs, and all across KC.
