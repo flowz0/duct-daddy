59a493a09dad9f5a8a57c0cf68fd0d79ce5cfa6c
flowz0 <billylflowers@gmail.com>
2025-06-11 12:44:46 -0700

    optimize not-found page Hero component for consistent UI/UX

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,24 +5,26 @@ import NotFoundImg from "@/public/404-not-found.png";
 
 export default function NotFoundPage() {
   return (
-    <main className="py-32 px-6 max-w-7xl mx-auto">
-      <header className="flex flex-col items-center">
-        <h1 className="text-3xl font-semibold max-w-sm text-center sm:text-4xl">
-          <span className="text-[#0080DB] font-bold">Page</span> not found
+    <main className="pt-40 pb-32 px-6 max-w-7xl mx-auto flex flex-col gap-8 md:pt-60 md:pb-32 lg:flex-row">
+      <header className="flex flex-col items-center lg:w-1/2 lg:items-start">
+        <h1 className="text-4xl font-bold max-w-xl text-center sm:text-5xl lg:text-start">
+          Page Not Found But Clean Air is A Click Away
         </h1>
-        <p className="max-w-xs text-center mt-4">This page does not exist. Please return to the homepage.</p>
-        <Link href="/" className="mt-12 bg-[#0080DB] text-[#E6E6E6] py-2 px-4 rounded-md hover:bg-[hsl(205,100%,33%)] active:bg-[hsl(205,100%,23%)]">
-          Return to homepage
+        <p className="mt-6 text-center max-w-lg lg:text-start">
+          Looks like the page you&apos;re searching for doesn&apos;t exist. But no worries, Duct Daddy Duct Cleaning is still here with expert HVAC and air duct cleaning services.
+        </p>
+        <Link href="/" className="bg-[#0080DB] text-[#E6E6E6] mt-12 py-2 px-4 rounded-md hover:bg-[hsl(205,100%,33%)] active:bg-[hsl(205,100%,23%)]">
+          Back to Fresh Air
         </Link>
       </header>
-      <section className="flex justify-center">
+      <div className="mt-8 flex justify-center lg:mt-0 lg:w-1/2">
         <Image
           src={NotFoundImg}
           alt="Page not found image"
-          className="mt-16 max-h-96 w-fit"
-          draggable={false}
+          className="max-h-96 w-auto rounded-3xl"
+          priority
         />
-      </section>
+      </div>
     </main>
   );
 }
