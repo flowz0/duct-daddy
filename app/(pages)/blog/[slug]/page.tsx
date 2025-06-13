47caf74ec20a30dd1dec9faa51917d4af2bcc84e
flowz0 <billylflowers@gmail.com>
2025-06-12 23:10:55 -0700

    add css to Blog slug page

--- app/(pages)/blog/[slug]/page.tsx
+++ app/(pages)/blog/[slug]/page.tsx
@@ -1,7 +1,9 @@
 export default function BlogPage() {
   return (
-    <article>
-      <h1>Blog Title</h1>
+    <article className="pt-40 pb-32 px-6 max-w-7xl mx-auto md:pt-40 md:pb-32">
+      <h1 className="text-4xl font-semibold text-center sm:text-5xl">
+        Blog Title
+      </h1>
     </article>
   );
 }
