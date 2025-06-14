8f9947245e39b651166a4335828af37376167bf4
flowz0 <billylflowers@gmail.com>
2025-06-13 22:29:06 -0700

    improve LatestBlogs component grid layout to be responsive in different viewports

--- app/components/pages/blog/LatestBlogs.tsx
+++ app/components/pages/blog/LatestBlogs.tsx
@@ -4,7 +4,7 @@ export default function LatestBlogs() {
   return (
     <section className="mt-12">
       <h2 className="text-2xl font-semibold md:text-3xl">Recent Blogs</h2>
-      <div className="mt-4 flex flex-col gap-8 md:flex-row">
+      <div className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         <BlogCard />
         <BlogCard />
         <BlogCard />
