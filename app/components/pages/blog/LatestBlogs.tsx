47b90f67500f3e2f07614bc29cfd550f6af26e06
flowz0 <billylflowers@gmail.com>
2025-06-15 00:32:00 -0700

    increase gap between BlogCard components in LatestBlogs component

--- app/components/pages/blog/LatestBlogs.tsx
+++ app/components/pages/blog/LatestBlogs.tsx
@@ -3,7 +3,7 @@ import BlogCard from "./BlogCard";
 export default function LatestBlogs() {
   return (
     <section className="mt-4">
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
         <BlogCard />
         <BlogCard />
         <BlogCard />
