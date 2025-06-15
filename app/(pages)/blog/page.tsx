cc82a3dfa36e2797a58270b8b6891ce169614610
flowz0 <billylflowers@gmail.com>
2025-06-15 12:28:49 -0700

    optimize blog page padding

--- app/(pages)/blog/page.tsx
+++ app/(pages)/blog/page.tsx
@@ -5,7 +5,7 @@ import FAQs from "@/app/components/pages/landing/FAQs";
 export default function Blog() {
   return (
     <>
-      <article className="pt-40 pb-32 px-6 max-w-7xl mx-auto md:pt-40 md:pb-32">
+      <article className="pt-32 pb-16 px-6 max-w-7xl mx-auto md:pt-40 md:pb-16">
         <header>
           <h1 className="text-4xl font-semibold text-center sm:text-5xl">
             Discover our latest blogs
