bf39029abf7c86f08047bf2843eb565480ccab87
flowz0 <billylflowers@gmail.com>
2025-06-15 15:19:52 -0700

    optimize Carousel component style customization

--- app/components/pages/landing/Carousel.tsx
+++ app/components/pages/landing/Carousel.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-import { BsArrowLeftCircleFill } from "react-icons/bs";
-import { BsArrowRightCircleFill } from "react-icons/bs";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 
 interface CarouselProps {
   images: string[];
@@ -15,40 +14,40 @@ interface CarouselProps {
 export default function Carousel({
   images,
   altTexts = [],
-  autoPlayInterval = 5000, // 5 seconds
+  // autoPlayInterval = 5000, // 5 seconds
 }: CarouselProps) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevImage = () => {
-    if (currentIndex !== 0) {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === 0 ? images.length - 1 : prevIndex - 1
-      );
-    }
+    // if (currentIndex !== 0) {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+    // }
   };
 
   const nextImage = () => {
-    if (currentIndex !== images.length - 1) {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
-    }
+    // if (currentIndex !== images.length - 1) {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+    // }
   };
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (currentIndex !== images.length - 1) {
-        setCurrentIndex((prevIndex) =>
-          prevIndex === images.length - 1 ? 0 : prevIndex + 1
-        );
-      }
-    }, autoPlayInterval);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     if (currentIndex !== images.length - 1) {
+  //       setCurrentIndex((prevIndex) =>
+  //         prevIndex === images.length - 1 ? 0 : prevIndex + 1
+  //       );
+  //     }
+  //   }, autoPlayInterval);
 
-    return () => clearInterval(interval);
-  }, [currentIndex, images.length, autoPlayInterval]);
+  //   return () => clearInterval(interval);
+  // }, [currentIndex, images.length, autoPlayInterval]);
 
   return (
-    <div className="relative w-full aspect-[16/9] rounded-md shadow overflow-hidden mt-12">
+    <div className="relative w-full aspect-[16/9] rounded-md shadow overflow-hidden mt-8">
       <Image
         src={images[currentIndex]}
         alt={altTexts[currentIndex] || `Image ${currentIndex + 1}`}
@@ -59,26 +58,24 @@ export default function Carousel({
         type="button"
         onClick={prevImage}
         aria-label="Previous image"
-        className={`absolute left-4 top-1/2 -translate-y-1/2 md:left-6 ${currentIndex !== 0 ? "text-[#0080DB] hover:text-[hsl(205,100%,33%)] active:text-[hsl(205,100%,23%)]" : "text-[hsl(0,0%,60%)] cursor-not-allowed"
-          }`}
+        className={`bg-[#0080DB] text-[#E6E6E6] absolute left-2 top-1/2 -translate-y-1/2 p-2 rounded-full shadow shadow-[hsl(0,0%,20%)] hover:bg-[hsl(205,100%,35%)] active:bg-[hsl(205,100%,30%)] md:left-6 md:p-3`}
       >
-        <BsArrowLeftCircleFill className="w-8 h-8 md:w-12 md:h-12" />
+        <FaArrowLeft className="w-4 h-4 md:w-6 md:h-6" />
       </button>
       <button
         type="button"
         onClick={nextImage}
         aria-label="Next image"
-        className={`absolute right-4 top-1/2 -translate-y-1/2 md:right-6 ${currentIndex !== images.length - 1 ? "text-[#0080DB] hover:text-[hsl(205,100%,33%)] active:text-[hsl(205,100%,23%)]" : "text-[hsl(0,0%,60%)] cursor-not-allowed"
-          }`}
+        className={`bg-[#0080DB] text-[#E6E6E6] absolute right-2 top-1/2 -translate-y-1/2 p-2 rounded-full shadow shadow-[hsl(0,0%,20%)] hover:bg-[hsl(205,100%,35%)] active:bg-[hsl(205,100%,30%)] md:right-6 md:p-3`}
       >
-        <BsArrowRightCircleFill className="w-8 h-8 md:w-12 md:h-12" />
+        <FaArrowRight className="w-4 h-4 md:w-6 md:h-6" />
       </button>
 
-      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-2 justify-center md:bottom-6">
+      <div className="absolute bottom-2 left-1/2 -translate-x-1/2 flex gap-2 justify-center md:bottom-6">
         {images.map((_, index) => (
           <div
             key={index}
-            className={`h-2 w-2 rounded-full ${index === currentIndex ? "bg-[#0080DB]" : "bg-[hsl(0,0%,60%)]"
+            className={`h-2 w-2 rounded-full shadow shadow-[hsl(0,0%,20%)] ${index === currentIndex ? "bg-[#0080DB]" : "bg-[#E6E6E6]"
               }`}
           />
         ))}
