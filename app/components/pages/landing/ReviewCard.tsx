1b6d9917b47bc5ae612d6803aedae1d87d0d993a
flowz0 <billylflowers@gmail.com>
2025-06-15 12:22:07 -0700

    improve ReviewCard component css

--- app/components/pages/landing/ReviewCard.tsx
+++ app/components/pages/landing/ReviewCard.tsx
@@ -13,32 +13,32 @@ export default function ReviewCard({ review, direction }: Props) {
 
   return (
     <motion.div
-      className="min-w-[300px] bg-[#333333] rounded-xl shadow shadow-[hsl(0,0%,20%)] py-10 px-8 md:min-w-[400px]"
+      className="min-w-[300px] bg-[#ffffff] text-[#333333] rounded-xl shadow shadow-[hsl(0,0%,80%)] py-10 px-8 md:min-w-[400px]"
       initial={{ opacity: 0, x: initialX }}
       whileInView={{ opacity: 1, x: 0 }}
       viewport={{ once: true }}
       transition={{ duration: 0.6, ease: "easeOut" }}
     >
       <div className="flex gap-0.5">
-        <BsStarFill className="text-amber-500 w-6 h-6" />
-        <BsStarFill className="text-amber-500 w-6 h-6" />
-        <BsStarFill className="text-amber-500 w-6 h-6" />
-        <BsStarFill className="text-amber-500 w-6 h-6" />
-        <BsStarFill className="text-amber-500 w-6 h-6" />
+        <BsStarFill className="text-amber-500 w-4 h-4" />
+        <BsStarFill className="text-amber-500 w-4 h-4" />
+        <BsStarFill className="text-amber-500 w-4 h-4" />
+        <BsStarFill className="text-amber-500 w-4 h-4" />
+        <BsStarFill className="text-amber-500 w-4 h-4" />
       </div>
-      <p className="text-[hsl(0,0%,80%)] text-lg mt-6">
+      <p className="text-[#333333] text-lg mt-6">
         &quot;{review.message}&quot;
       </p>
       <div className="flex items-center gap-4 mt-6">
         <Image
           src={review.avatar}
           alt={review.name}
-          className="w-12 h-12 rounded-full"
+          className="w-10 h-10 rounded-full"
           width={64}
           height={64}
         />
         <div>
-          <p className="text-[hsl(0,0%,80%)]">{review.name}</p>
+          <p className="text-[hsl(0,0%,40%)]">{review.name}</p>
           {review.brand && (
             <p className="text-[hsl(0,0%,60%)]">{review.brand}</p>
           )}
