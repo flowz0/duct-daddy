8753f00f7464e291097a60803411ff86fb9a5f22
flowz0 <billylflowers@gmail.com>
2025-06-15 15:17:02 -0700

    improve blog slug page responsive spacing

--- app/(pages)/blog/[slug]/page.tsx
+++ app/(pages)/blog/[slug]/page.tsx
@@ -7,7 +7,7 @@ import LatestBlogs from "@/app/components/pages/blog/LatestBlogs";
 export default function BlogPage() {
   return (
     <>
-      <article className="pt-32 pb-16 px-6 max-w-7xl mx-auto md:pt-40 md:pb-16">
+      <article className="pt-32 pb-16 px-6 max-w-7xl mx-auto md:pt-36">
         <header>
           <h1 className="text-4xl max-w-4xl mx-auto font-semibold text-center sm:text-5xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, pariatur.
@@ -16,23 +16,23 @@ export default function BlogPage() {
           <Image
             src={PlaceholderImg}
             alt="Blog image"
-            className="mt-16 h-full w-auto object-cover rounded-lg"
+            className="mt-12 h-full w-auto object-cover rounded-lg md:mt-16"
             draggable={false}
           />
         </header>
-        <section className="mt-12 max-w-2xl mx-auto">
+        <section className="mt-8 max-w-2xl mx-auto md:mt-12">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi maiores cum ratione obcaecati neque nobis quo officiis, est aliquid possimus architecto laudantium ad. Possimus a dolorem temporibus, provident aperiam corrupti.</p>
         </section>
-        <section className="mt-12 max-w-2xl mx-auto">
+        <section className="mt-8 max-w-2xl mx-auto md:mt-12">
           <h2 className="text-3xl font-semibold sm:text-4xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
           <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi maiores cum ratione obcaecati neque nobis quo officiis, est aliquid possimus architecto laudantium ad. Possimus a dolorem temporibus, provident aperiam corrupti.</p>
         </section>
-        <section className="mt-12 max-w-2xl mx-auto">
+        <section className="mt-8 max-w-2xl mx-auto md:mt-12">
           <h2 className="text-3xl font-semibold sm:text-4xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
           <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi maiores cum ratione obcaecati neque nobis quo officiis, est aliquid possimus architecto laudantium ad. Possimus a dolorem temporibus, provident aperiam corrupti.</p>
         </section>
       </article>
-      <div className="py-32 px-6 max-w-7xl mx-auto">
+      <div className="py-16 px-6 max-w-7xl mx-auto">
         <h2 className="mt-12 text-2xl font-semibold md:text-3xl">Other Blogs</h2>
         <LatestBlogs />
       </div>
