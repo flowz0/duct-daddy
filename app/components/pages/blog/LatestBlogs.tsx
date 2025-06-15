bc0f14df074d569bcdb23680b8849b93eb1bb917
flowz0 <billylflowers@gmail.com>
2025-06-14 18:31:31 -0700

    Remove title from LatestBlogs component

--- app/components/pages/blog/LatestBlogs.tsx
+++ app/components/pages/blog/LatestBlogs.tsx
@@ -2,9 +2,8 @@ import BlogCard from "./BlogCard";
 
 export default function LatestBlogs() {
   return (
-    <section className="mt-12">
-      <h2 className="text-2xl font-semibold md:text-3xl">Recent Blogs</h2>
-      <div className="mt-4 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <section className="mt-4">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         <BlogCard />
         <BlogCard />
         <BlogCard />
