e9ee65493b69c4afa28c5c9c87a3ce0ebf137f46
flowz0 <billylflowers@gmail.com>
2025-06-14 18:32:27 -0700

    create blog slug page layout and BlogFeatures component

--- app/(pages)/blog/[slug]/page.tsx
+++ app/(pages)/blog/[slug]/page.tsx
@@ -1,9 +1,41 @@
+import BlogFeatures from "@/app/components/pages/blog/BlogFeatures";
+import Image from "next/image";
+import PlaceholderImg from "@/public/1920x1080.svg";
+import CTA from "@/app/components/CTA";
+import LatestBlogs from "@/app/components/pages/blog/LatestBlogs";
+
 export default function BlogPage() {
   return (
-    <article className="pt-40 pb-32 px-6 max-w-7xl mx-auto md:pt-40 md:pb-32">
-      <h1 className="text-4xl font-semibold text-center sm:text-5xl">
-        Blog Title
-      </h1>
-    </article>
+    <>
+      <article className="pt-40 pb-32 px-6 max-w-7xl mx-auto md:pt-40 md:pb-32">
+        <header>
+          <h1 className="text-4xl max-w-4xl mx-auto font-semibold text-center sm:text-5xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, pariatur.
+          </h1>
+          <BlogFeatures />
+          <Image
+            src={PlaceholderImg}
+            alt="Blog image"
+            className="mt-16 h-full w-auto object-cover rounded-lg"
+          />
+        </header>
+        <section className="mt-12 max-w-2xl mx-auto">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi maiores cum ratione obcaecati neque nobis quo officiis, est aliquid possimus architecto laudantium ad. Possimus a dolorem temporibus, provident aperiam corrupti.</p>
+        </section>
+        <section className="mt-12 max-w-2xl mx-auto">
+          <h2 className="text-3xl font-semibold sm:text-4xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi maiores cum ratione obcaecati neque nobis quo officiis, est aliquid possimus architecto laudantium ad. Possimus a dolorem temporibus, provident aperiam corrupti.</p>
+        </section>
+        <section className="mt-12 max-w-2xl mx-auto">
+          <h2 className="text-3xl font-semibold sm:text-4xl">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+          <p className="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi maiores cum ratione obcaecati neque nobis quo officiis, est aliquid possimus architecto laudantium ad. Possimus a dolorem temporibus, provident aperiam corrupti.</p>
+        </section>
+      </article>
+      <div className="py-32 px-6 max-w-7xl mx-auto">
+        <h2 className="mt-12 text-2xl font-semibold md:text-3xl">Other Blogs</h2>
+        <LatestBlogs />
+      </div>
+      <CTA />
+    </>
   );
 }
---
+++ app/components/pages/blog/BlogFeatures.tsx
@@ -0,0 +1,26 @@
+import { FaUser, FaCalendarDays, FaClock } from "react-icons/fa6";
+
+export default function BlogFeatures() {
+  return (
+    <section className="flex flex-wrap gap-x-8 gap-y-4 justify-center items-center mt-8">
+      <div className="flex items-center gap-2.5">
+        <FaUser className="text-[#0080DB] w-4 h-4" />
+        <h4 className="font-semibold">
+          Karson Kolle
+        </h4>
+      </div>
+      <div className="flex items-center gap-2.5">
+        <FaCalendarDays className="text-[#0080DB] w-4 h-4" />
+        <p className="font-semibold">
+          June 14, 2025
+        </p>
+      </div>
+      <div className="flex items-center gap-2.5">
+        <FaClock className="text-[#0080DB] w-4 h-4" />
+        <p className="font-semibold">
+          8 min read
+        </p>
+      </div>
+    </section>
+  );
+}
