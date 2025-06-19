09332cabfe81724d6c53fce6911acbc112872c83
flowz0 <billylflowers@gmail.com>
2025-06-19 12:22:32 -0700

    optimize CTA component

--- app/components/CTA.tsx
+++ app/components/CTA.tsx
@@ -2,25 +2,7 @@ import Link from "next/link";
 
 export default function CTA() {
   return (
-    // <section className="py-32 px-6 max-w-7xl mx-auto">
-    //   <div className="grid grid-cols-1 lg:grid-cols-2">
-    //     <div className="bg-[#ffffff] shadow shadow-[hsl(0,0%,80%)] py-8 px-6 flex flex-col justify-between lg:items-center rounded-3xl lg:flex-row md:py-16 md:px-12">
-    //       <div className="flex flex-col">
-    //         <h2 className="text-3xl font-semibold sm:text-4xl">
-    //           Breathe without worries
-    //         </h2>
-    //         <p className="mt-4 max-w-xl">
-    //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eaque, amet, eius non veniam excepturi consequuntur animi quos eos in, doloremque magni numquam ipsam inventore esse sunt harum exercitationem quis!
-    //         </p>
-    //       </div>
-    //       <Link href="/book-online" className="bg-[#0080DB] text-[#E6E6E6] mt-8 py-6 px-12 text-lg text-center font-semibold rounded-md w-full lg:mt-0 hover:bg-[hsl(205,100%,33%)] active:bg-[hsl(205,100%,23%)]">
-    //         Book Online
-    //       </Link>
-    //     </div>
-    //   </div>
-    // </section>
-
-    <section className="py-32 px-6 max-w-7xl mx-auto">
+    <section className="pt-28 px-6 max-w-7xl mx-auto lg:pt-48">
       <div className="bg-[#ffffff] shadow shadow-[hsl(0,0%,80%)] py-8 px-6 rounded-xl flex flex-col items-center sm:py-16 sm:px-12">
         <header className="flex flex-col">
           <h2 className="text-3xl font-semibold text-center md:text-4xl">
