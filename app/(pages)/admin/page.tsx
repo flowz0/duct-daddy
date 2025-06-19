286dcbbd85c1e032e12392a7cb22f98bfb8d6cbf
flowz0 <billylflowers@gmail.com>
2025-06-19 12:21:58 -0700

    optimize LoginPage

--- app/(pages)/admin/page.tsx
+++ app/(pages)/admin/page.tsx
@@ -1,28 +1,16 @@
 import LoginForm from "@/app/components/pages/admin/LoginForm";
-import Image from "next/image";
-
-import DuctDaddyLogo from "@/public/Duct-Daddy-03.png";
 
 export default function LoginPage() {
   return (
     <main className="pt-32 pb-16 px-6 max-w-7xl mx-auto">
       <div className="bg-[#ffffff] shadow shadow-[hsl(0,0%,80%)] py-12 px-6 rounded-xl md:p-16">
-        <div className="flex justify-center items-center gap-6 md:justify-start">
-          <Image
-            src={DuctDaddyLogo}
-            alt="Duct Daddy Logo"
-            className="h-12 w-auto object-cover"
-            draggable={false}
-          />
-          <p className="text-lg font-semibold md:text-xl">Blog CMS</p>
-        </div>
-        <div className="mt-12 flex flex-col gap-8 md:flex-row">
+        <div className="flex flex-col gap-8 md:flex-row">
           <header className="w-full md:w-1/2">
             <h1 className="text-4xl font-semibold text-center md:text-5xl md:text-start">
               Log in
             </h1>
             <p className="mt-4 text-center md:text-start">
-              Access the blog content management system
+              Access the Duct Daddy Blog CMS.
             </p>
           </header>
 
