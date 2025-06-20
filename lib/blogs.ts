c805e37d76911a5307c5368f3872e4cc5bcea7dc
flowz0 <billylflowers@gmail.com>
2025-06-20 16:14:48 -0700

    add new blogs data to test Blogs filter

--- lib/blogs.ts
+++ lib/blogs.ts
@@ -94,5 +94,18 @@ export async function getBlogs(): Promise<BlogProps[]> {
       header2: "Header two for this blog post",
       paragraph2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
     },
+    {
+      id: "8",
+      author: "Karson Kolle",
+      createdAt: "2025-06-20T18:40:00.000Z",
+      readTime: 1,
+      title: "My 2First Blog",
+      img: BlogImgPlaceholder,
+      summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      header: "This is the first blog header",
+      paragraph: "This is the first blog paragraph for this post. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      header2: "Header two for this blog post",
+      paragraph2: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    },
   ];
 }
