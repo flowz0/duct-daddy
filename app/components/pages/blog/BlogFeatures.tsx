edaa766faec1e7f16994e7abc649908db4a21d81
flowz0 <billylflowers@gmail.com>
2025-06-20 15:37:46 -0700

    create BlogFeaturesProps and pass blog data into BlogFeatures component

--- app/components/pages/blog/BlogFeatures.tsx
+++ app/components/pages/blog/BlogFeatures.tsx
@@ -1,24 +1,31 @@
+import formatDate from "@/lib/formatDate";
 import { FaUser, FaCalendarDays, FaClock } from "react-icons/fa6";
 
-export default function BlogFeatures() {
+interface BlogFeaturesProps {
+  author: string;
+  date: string;
+  readTime: number;
+}
+
+export default function BlogFeatures({author, date, readTime}: BlogFeaturesProps) {
   return (
     <section className="flex flex-wrap justify-center items-center gap-x-6 gap-y-4 mt-8 md:gap-x-12">
       <div className="flex items-center gap-2.5">
         <FaUser className="text-[#0080DB] w-4 h-4" />
         <h4 className="font-semibold">
-          Karson Kolle
+          {author}
         </h4>
       </div>
       <div className="flex items-center gap-2.5">
         <FaCalendarDays className="text-[#0080DB] w-4 h-4" />
         <p className="font-semibold">
-          June 14, 2025
+          {formatDate(date)}
         </p>
       </div>
       <div className="flex items-center gap-2.5">
         <FaClock className="text-[#0080DB] w-4 h-4" />
         <p className="font-semibold">
-          8 min read
+          {readTime} min read
         </p>
       </div>
     </section>
