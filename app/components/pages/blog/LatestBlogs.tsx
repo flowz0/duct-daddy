7212220d54cb5aa7ab20bc5ddea88bf12230116b
flowz0 <billylflowers@gmail.com>
2025-06-20 14:46:16 -0700

    sort blogs by createdAt in descending order, slice only 3 blogs, map over latestBlogs

--- app/components/pages/blog/LatestBlogs.tsx
+++ app/components/pages/blog/LatestBlogs.tsx
@@ -1,12 +1,24 @@
+import { getBlogs } from "@/lib/blogs";
 import BlogCard from "./BlogCard";
+import { BlogProps } from "@/types/blog";
 
-export default function LatestBlogs() {
+export default async function LatestBlogs() {
+  const blogs = await getBlogs();
+
+  // Sort blogs by createdAt in descending order
+  const sortedBlogs: BlogProps[] = blogs.sort(
+    (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+  );
+
+  // Take only the top 3 most recent blogs
+  const latestBlogs = sortedBlogs.slice(0, 3);
+
   return (
     <section className="mt-2">
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
+      <div className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 lg:grid-cols-3">
+        {latestBlogs.map((blog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
       </div>
     </section>
   );
