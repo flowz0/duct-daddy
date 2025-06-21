ccf66775f444b6f3504a38585120fab32110eb6c
flowz0 <billylflowers@gmail.com>
2025-06-21 01:34:08 -0700

    remove white bg color from Login and Register Forms

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -3,19 +3,16 @@ import LoginForm from "@/app/components/pages/dashboard/LoginForm";
 export default function LoginPage() {
   return (
     <main className="pt-32 pb-16 px-6 max-w-7xl mx-auto lg:pt-40">
-      <div className="bg-[#ffffff] shadow shadow-[hsl(0,0%,80%)] py-12 px-6 rounded-xl md:p-16">
-        <div className="flex flex-col gap-8 md:flex-row">
-          <header className="w-full md:w-1/2">
-            <h1 className="text-4xl font-semibold text-center md:text-5xl md:text-start">
-              Log in
-            </h1>
-            <p className="mt-4 text-center md:text-start">
-              Access the Duct Daddy Blog CMS.
-            </p>
-          </header>
-
-          <LoginForm />
-        </div>
+      <div className="flex flex-col gap-8 md:flex-row">
+        <header className="w-full md:w-1/2">
+          <h1 className="text-4xl font-semibold text-center md:text-5xl md:text-start">
+            Log in
+          </h1>
+          <p className="mt-4 text-center md:text-start">
+            Access the Duct Daddy Blog CMS.
+          </p>
+        </header>
+        <LoginForm />
       </div>
     </main>
   );
