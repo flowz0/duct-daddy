d9cfe0fd78e084dbc0aa71425f248ad112baec17
flowz0 <billylflowers@gmail.com>
2025-06-22 16:57:33 -0700

    refactor CreateBlogPage Header and implement CreateBlogForm component

--- app/(pages)/create-blog/page.tsx
+++ app/(pages)/create-blog/page.tsx
@@ -1,17 +1,13 @@
-import PostBlogForm from "@/app/components/pages/dashboard/PostBlogForm";
+import Header from "@/app/components/Header";
+import CreateBlogForm from "@/app/components/pages/dashboard/CreateBlogForm";
 
 export default function CreateBlogPage() {
   return (
-    <main className="pt-32 pb-16 px-6 max-w-2xl mx-auto lg:pt-40">
-      <header>
-        <h1 className="text-4xl font-semibold text-center sm:text-5xl">
-          Post a Blog
-        </h1>
-        <p className="mt-4 max-w-xl mx-auto text-center">
-          Create a blog post by filling out the following fields.
-        </p>
-      </header>
-      <PostBlogForm />
+    <main className="mb-16">
+      <Header subtitle="Create a blog post by filling out the following fields.">
+        Post a blog
+      </Header>
+      <CreateBlogForm />
     </main>
   );
 }
