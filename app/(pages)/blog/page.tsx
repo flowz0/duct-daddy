bb277fbf5129a8afd77d27e0f45748c5c04ee27c
flowz0 <billylflowers@gmail.com>
2025-06-22 16:56:35 -0700

    refactor Blog Header and implement AllBlogs component

--- app/(pages)/blog/page.tsx
+++ app/(pages)/blog/page.tsx
@@ -1,23 +1,28 @@
 import CTA from "@/app/components/CTA";
-import LatestBlogs from "@/app/components/pages/blog/LatestBlogs";
+import AllBlogs from "@/app/components/pages/blog/AllBlogs";
+import FeaturedBlogs from "@/app/components/pages/blog/FeaturedBlogs";
+import Header from "@/app/components/Header";
 import FAQs from "@/app/components/pages/landing/FAQs";
 
 export default function Blog() {
   return (
     <>
-      <article className="bg-[#ffffff] pt-32 px-6 lg:pt-40">
-        <div className="max-w-7xl mx-auto">
-          <header>
-            <h1 className="text-4xl font-semibold text-center sm:text-5xl">
-              Discover our latest blogs
-            </h1>
-            <p className="mt-4 max-w-xl mx-auto text-center">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam quas ab vero nostrum, illo quam ullam nisi, ea aspernatur voluptas officiis porro tempora adipisci amet inventore ipsa earum! Cum, doloremque?
-            </p>
-          </header>
-          <div className="pt-12 pb-32">
-            <h2 className="text-2xl font-semibold md:text-3xl">Recent Blogs</h2>
-            <LatestBlogs />
+      <article>
+        <div>
+          <Header>
+            Discover our latest blogs
+          </Header>
+          <div className="bg-[#ffffff] px-6">
+            <div className="py-20 flex flex-col gap-y-16 max-w-7xl mx-auto">
+              <div>
+                <h2 className="text-2xl font-semibold md:text-3xl">Featured blogs</h2>
+                <FeaturedBlogs />
+              </div>
+              <div>
+                <h2 className="text-2xl font-semibold md:text-3xl">All blogs</h2>
+                <AllBlogs />
+              </div>
+            </div>
           </div>
         </div>
       </article>
