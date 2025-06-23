4934597255a98c626cfe9263f6fa739887d1fd50
flowz0 <billylflowers@gmail.com>
2025-06-22 17:15:27 -0700

    improve FileInput component style customization

--- app/components/pages/dashboard/FileInput.tsx
+++ app/components/pages/dashboard/FileInput.tsx
@@ -37,15 +37,15 @@ export default function FileInput({
         onBlur={onBlur}
         className="hidden"
       />
-      <div className={`text-[#333333] mt-2 rounded-lg outline-none flex items-center gap-x-4 w-fit `}>
+      <div className={`text-[#333333] mt-2 rounded-lg outline-none flex items-center gap-x-4 w-fit`}>
         <label
           htmlFor={name}
-          className={`cursor-pointer bg-[#0080DB] text-white px-4 py-2 rounded-md font-medium hover:bg-[#006bbf] ${error ? "ring-2 ring-offset-2 ring-red-500" : "focus:ring-2 focus:ring-offset-2 focus:ring-[#0080DB]"
+          className={`cursor-pointer bg-[#0080DB] text-white px-4 py-2 rounded-md font-semibold hover:bg-[#006bbf] ${error ? "ring-2 ring-offset-2 ring-red-500" : "focus:ring-2 focus:ring-offset-2 focus:ring-[#0080DB]"
             }`}
         >
           Choose image
         </label>
-        <span className="text-sm text-gray-600">
+        <span className="text-sm text-[hsl(0,0%,40%)]">
           {previewUrl ? "Image selected" : "No image chosen"}
         </span>
       </div>
@@ -55,7 +55,7 @@ export default function FileInput({
           alt="Preview"
           height={1920}
           width={1080}
-          className="mt-3 object-cover rounded-md ring-4 ring-offset-2 ring-[#0080DB] sm:max-w-[200px]"
+          className="mt-4 object-cover rounded-md ring-4 ring-offset-2 ring-[#0080DB] sm:max-w-[200px] sm:ml-1.5"
         />
       )}
       {error && <p className="text-sm text-red-600 mt-2">{error}</p>}
