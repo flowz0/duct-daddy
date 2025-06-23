dbd1e410f92a9a259e78a16ed7c1e0aa17d3b2e1
flowz0 <billylflowers@gmail.com>
2025-06-22 17:01:40 -0700

    update FeaturedBlogs component name

--- app/components/pages/blog/FeaturedBlogs.tsx
+++ app/components/pages/blog/FeaturedBlogs.tsx
@@ -2,18 +2,18 @@ import { getBlogs } from "@/lib/blogs";
 import BlogCard from "./BlogCard";
 import { BlogProps } from "@/types/blog";
 
-export default async function LatestBlogs() {
+export default async function FeaturedBlogs() {
   const blogs = await getBlogs();
 
-  const latestBlogs = blogs
+  const featuredBlogs = blogs
     .filter((b): b is BlogProps => !!b.createdAt)
     .sort((a,b) => new Date(b.createdAt!).getTime() - new Date(a.createdAt!).getTime())
     .slice(0, 3);
 
   return (
-    <section className="mt-2">
+    <section className="mt-4">
       <div className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 lg:grid-cols-3">
-        {latestBlogs.map((blog) => (
+        {featuredBlogs.map((blog) => (
           <BlogCard key={blog.id} blog={blog} />
         ))}
       </div>
