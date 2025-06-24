6e41c0a7ff15739704335092de2cdeab495fb376
flowz0 <billylflowers@gmail.com>
2025-06-23 21:34:42 -0700

    create and implement MobileCallBtn component so mobile users can call Duct Daddy

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,12 @@ import ReviewSection from "./components/pages/landing/ReviewSection";
 import ServiceArea from "./components/pages/landing/ServiceArea";
 import FAQs from "./components/pages/landing/FAQs";
 import CTA from "./components/CTA";
+import MobileCallBtn from "./components/MobileCallBtn";
 
 export default function Home() {
   return (
     <main>
+      <MobileCallBtn />
       <Hero />
       <Stats />
       <Services />
